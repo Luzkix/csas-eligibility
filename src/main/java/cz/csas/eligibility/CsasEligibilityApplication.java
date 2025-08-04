f2297fbde9ce8381ec853b2a2ead2d34ce784cf7
Zdenek L <66368980+Luzkix@users.noreply.github.com>
2025-08-04 17:13:59 +0200

    Initial project structure

---
+++ src/main/java/cz/csas/eligibility/CsasEligibilityApplication.java
@@ -0,0 +1,13 @@
+package cz.csas.eligibility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsasEligibilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsasEligibilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/csas/eligibility/CsasEligibilityApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.csas.eligibility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsasEligibilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
