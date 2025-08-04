2a27f0860c5ce2280d85bfae218bc317046c4e70
Zdenek L <66368980+Luzkix@users.noreply.github.com>
2025-08-04 20:49:37 +0200

    - API_definition.yaml -> changed servers definitions - they are now defined using "variables" which can be easily redefined within pom.xml. This allows to autogenerate 3 different basePaths from a single yaml file with the help of <serverVariableOverrides> property in pom.xml - pom.xml -> added auto generation of all 3 API which will be used for bot internal API calls (by frontend) and external API calls (clients API, accounts API). Models are generated just once and are shared(used) between all 3 apis

--- src/main/java/cz/csas/eligibility/CsasEligibilityApplication.java
+++ src/main/java/cz/csas/eligibility/CsasEligibilityApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CsasEligibilityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CsasEligibilityApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CsasEligibilityApplication.class, args);
+    }
 
 }
